11d30a18d4c32bd794c532f292400b5573d94e09
Joe Bowman <bowman.joseph@gmail.com>
2013-03-16 18:02:11 -0400

    ok inline docs and starting to use gofmt. Expect lots of commits from me, that's what I do.

--- api/server.go
+++ api/server.go
@@ -6,19 +6,24 @@ import (
 	"net/http"
 )
 
+// Root of the api, at some point this might be a good place
+// to document the api for developers.
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the fluffcut api")
 }
 
+// This is the base for the login. Apps should be able to direct
+// applications to this end point for login. This documentation
+// will be flushed out a lot more.
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "login will happen here")
 }
 
 func main() {
 	r := mux.NewRouter()
-        r.StrictSlash(true)
+	r.StrictSlash(true)
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/login", LoginHandler)
 	http.Handle("/", r)
-        http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
