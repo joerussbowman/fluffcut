f84fdcb722c51d8e385d2434331726968522a2da
Joe Bowman <bowman.joseph@gmail.com>
2013-03-16 17:54:17 -0400

    more playing with the router

--- api/server.go
+++ api/server.go
@@ -10,9 +10,15 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the fluffcut api")
 }
 
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "login will happen here")
+}
+
 func main() {
 	r := mux.NewRouter()
+        r.StrictSlash(true)
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/login", LoginHandler)
 	http.Handle("/", r)
         http.ListenAndServe(":8080", nil)
 }
